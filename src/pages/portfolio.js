584afdec13d3f7088bbe31f03fd50aa41a22de1f
Ethan Elsop <67030991+erelsop@users.noreply.github.com>
2022-04-27 22:15:32 -0400

    Add item to portfolio

--- src/pages/portfolio.js
+++ src/pages/portfolio.js
@@ -48,6 +48,26 @@ const portfolio = () => {
           GitHub Page
         </a>
       </section>
+      <section className="portfolio-item-container">
+        <h1 className="item-title">Class Registration System</h1>
+        <p className="item-description">
+          A project assignment for my Python programming class. The objective
+          was to create a class registration system utilizing object-oriented
+          design. It allows users to log in as students or administrators.
+          Students can add courses, drop courses, and list courses they are
+          registered for. Administrators can show class rosters and change class
+          sizes.
+        </p>
+        <p className="item-language">Language: Python</p>
+        <a
+          href="https://github.com/erelsop/class-registration-system"
+          className="item-link"
+          target="_blank"
+          rel="noreferrer"
+        >
+          GitHub Page
+        </a>
+      </section>
     </section>
   )
 }
